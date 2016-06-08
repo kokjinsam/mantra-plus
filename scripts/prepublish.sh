1402ec316a79f54e49b3db6138fac722cf59b730
skaxwng <samkj.ks@gmail.com>
2016-06-08 15:13:15 -0500

    Updated prepublish

--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -1,5 +1,6 @@
 echo "> Start transpiling ES2015"
 echo ""
+rm -rf ./lib
 ./node_modules/.bin/babel --plugins "transform-runtime" src --ignore __tests__ --out-dir ./lib
 echo ""
 echo "> Complete transpiling ES2015"
